8d12fa488bfbd4dc1af16d5c1a866d4c620d5442
BenasB <29711974+BenasB@users.noreply.github.com>
2024-02-10 22:00:58 +0200

    Format migration

--- services/GroupMaker/GroupMaker.Data/Migrations/20240210191525_InitialMigration.cs
+++ services/GroupMaker/GroupMaker.Data/Migrations/20240210191525_InitialMigration.cs
@@ -16,27 +16,46 @@ namespace GroupMaker.Data.Migrations
                 name: "Groups",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "integer", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    StartTime = table.Column<TimeOnly>(type: "time without time zone", nullable: false),
+                    Id = table
+                        .Column<int>(type: "integer", nullable: false)
+                        .Annotation(
+                            "Npgsql:ValueGenerationStrategy",
+                            NpgsqlValueGenerationStrategy.IdentityByDefaultColumn
+                        ),
+                    StartTime = table.Column<TimeOnly>(
+                        type: "time without time zone",
+                        nullable: false
+                    ),
                     Days = table.Column<int>(type: "integer", nullable: false),
-                    StartLocation_Latitude = table.Column<double>(type: "double precision", nullable: false),
-                    StartLocation_Longitude = table.Column<double>(type: "double precision", nullable: false),
-                    EndLocation_Latitude = table.Column<double>(type: "double precision", nullable: false),
-                    EndLocation_Longitude = table.Column<double>(type: "double precision", nullable: false),
+                    StartLocation_Latitude = table.Column<double>(
+                        type: "double precision",
+                        nullable: false
+                    ),
+                    StartLocation_Longitude = table.Column<double>(
+                        type: "double precision",
+                        nullable: false
+                    ),
+                    EndLocation_Latitude = table.Column<double>(
+                        type: "double precision",
+                        nullable: false
+                    ),
+                    EndLocation_Longitude = table.Column<double>(
+                        type: "double precision",
+                        nullable: false
+                    ),
                     TotalSeats = table.Column<int>(type: "integer", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Groups", x => x.Id);
-                });
+                }
+            );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Groups");
+            migrationBuilder.DropTable(name: "Groups");
         }
     }
 }
