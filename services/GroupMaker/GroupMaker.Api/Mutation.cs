53d2e745c71ea073b2e76b6a84820b790673f109
BenasB <29711974+BenasB@users.noreply.github.com>
2024-02-13 00:16:25 +0200

    Adjust mutations and add group joining and abandoning mutations

--- services/GroupMaker/GroupMaker.Api/Mutation.cs
+++ services/GroupMaker/GroupMaker.Api/Mutation.cs
@@ -10,8 +10,10 @@ namespace GroupMaker.Api;
 [Authorize]
 public class Mutation
 {
+    [UseFirstOrDefault]
+    [UseProjection]
     [Error<AuthenticationException>]
-    public async Task<Group> CreateGroup(
+    public async Task<IQueryable<Group>> CreateGroup(
         TimeOnly startTime,
         DaysOfWeek days,
         Coordinates startLocation,
@@ -28,7 +30,7 @@ public class Mutation
 
         var driverUser = await context.Users.FirstOrDefaultAsync(
             u => u.Id == driverId,
-            cancellationToken: cancellationToken
+            cancellationToken
         );
 
         if (driverUser == null)
@@ -50,23 +52,117 @@ public class Mutation
         await context.Groups.AddAsync(group, cancellationToken);
         await context.SaveChangesAsync(cancellationToken);
 
-        return group;
+        return context.Groups.Where(x => x == group);
     }
 
+    [Error<AuthenticationException>]
     [Error<ArgumentException>]
-    public async Task<Group?> DeleteGroup(
+    [UseMutationConvention(PayloadFieldName = "id")]
+    public async Task<int> DeleteGroup(
         int id,
         GroupContext context,
+        ClaimsPrincipal claimsPrincipal,
         CancellationToken cancellationToken
     )
     {
+        var userId =
+            claimsPrincipal.Identity?.Name
+            ?? throw new AuthenticationException("Failed to determine the current user's name");
+
+        var user =
+            await context.Users.FirstOrDefaultAsync(u => u.Id == userId, cancellationToken)
+            ?? throw new ArgumentException("User does not exist");
+
         var group =
             await context.Groups.FirstOrDefaultAsync(x => x.Id == id, cancellationToken)
             ?? throw new ArgumentException("Group does not exist", nameof(id));
 
+        if (group.Driver != user)
+            throw new ArgumentException("User is not the driver");
+
         context.Groups.Remove(group);
         await context.SaveChangesAsync(cancellationToken);
 
-        return group;
+        return group.Id;
+    }
+
+    [UseFirstOrDefault]
+    [UseProjection]
+    [Error<AuthenticationException>]
+    [Error<ArgumentException>]
+    public async Task<IQueryable<Group>> JoinGroup(
+        int id,
+        GroupContext context,
+        ClaimsPrincipal claimsPrincipal,
+        CancellationToken cancellationToken
+    )
+    {
+        var userId =
+            claimsPrincipal.Identity?.Name
+            ?? throw new AuthenticationException("Failed to determine the current user's name");
+
+        var user = await context.Users.FirstOrDefaultAsync(u => u.Id == userId, cancellationToken);
+
+        if (user == null)
+        {
+            user = new User { Id = userId };
+            await context.Users.AddAsync(user, cancellationToken);
+        }
+
+        var group =
+            await context
+                .Groups.Include(group => group.Driver)
+                .Include(group => group.Passengers)
+                .FirstOrDefaultAsync(x => x.Id == id, cancellationToken)
+            ?? throw new ArgumentException("Group does not exist", nameof(id));
+
+        if (user == group.Driver)
+            throw new ArgumentException("User is the driver");
+
+        if (group.Passengers.Any(passenger => passenger == user))
+            throw new ArgumentException("User is already one of the passengers");
+
+        group.Passengers.Add(user);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return context.Groups.Where(x => x.Id == id);
+    }
+
+    [UseFirstOrDefault]
+    [UseProjection]
+    [Error<AuthenticationException>]
+    [Error<ArgumentException>]
+    public async Task<IQueryable<Group>> AbandonGroup(
+        int id,
+        GroupContext context,
+        ClaimsPrincipal claimsPrincipal,
+        CancellationToken cancellationToken
+    )
+    {
+        var userId =
+            claimsPrincipal.Identity?.Name
+            ?? throw new AuthenticationException("Failed to determine the current user's name");
+
+        var user =
+            await context.Users.FirstOrDefaultAsync(u => u.Id == userId, cancellationToken)
+            ?? throw new ArgumentException("User does not exist");
+
+        var group =
+            await context
+                .Groups.Include(group => group.Driver)
+                .Include(group => group.Passengers)
+                .FirstOrDefaultAsync(x => x.Id == id, cancellationToken)
+            ?? throw new ArgumentException("Group does not exist", nameof(id));
+
+        if (user == group.Driver)
+            throw new ArgumentException("User is the driver");
+
+        if (!group.Passengers.Any(passenger => passenger == user))
+            throw new ArgumentException("User is not one of the passengers");
+
+        group.Passengers.Remove(user);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return context.Groups.Where(x => x.Id == id);
     }
 }
