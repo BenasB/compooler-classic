db8dff50d5d702d7e5f2f9e1936474c8c92c4bdd
Benas Budrys <29711974+BenasB@users.noreply.github.com>
2024-02-13 00:37:58 +0200

    Add user entity, more work on mutations (#26)
    * Introduce users
    
    * Adjust mutations and add group joining and abandoning mutations
    
    * Add seat check

--- services/GroupMaker/GroupMaker.Api/Mutation.cs
+++ services/GroupMaker/GroupMaker.Api/Mutation.cs
@@ -1,4 +1,6 @@
-﻿using GroupMaker.Data;
+﻿using System.Security.Authentication;
+using System.Security.Claims;
+using GroupMaker.Data;
 using GroupMaker.Data.Entities;
 using HotChocolate.Authorization;
 using Microsoft.EntityFrameworkCore;
@@ -8,16 +10,35 @@ namespace GroupMaker.Api;
 [Authorize]
 public class Mutation
 {
-    public async Task<Group> CreateGroup(
+    [UseFirstOrDefault]
+    [UseProjection]
+    [Error<AuthenticationException>]
+    public async Task<IQueryable<Group>> CreateGroup(
         TimeOnly startTime,
         DaysOfWeek days,
         Coordinates startLocation,
         Coordinates endLocation,
         int totalSeats,
         GroupContext context,
+        ClaimsPrincipal claimsPrincipal,
         CancellationToken cancellationToken
     )
     {
+        var driverId =
+            claimsPrincipal.Identity?.Name
+            ?? throw new AuthenticationException("Failed to determine the current user's name");
+
+        var driverUser = await context.Users.FirstOrDefaultAsync(
+            u => u.Id == driverId,
+            cancellationToken
+        );
+
+        if (driverUser == null)
+        {
+            driverUser = new User { Id = driverId };
+            await context.Users.AddAsync(driverUser, cancellationToken);
+        }
+
         var group = new Group()
         {
             StartTime = startTime,
@@ -25,28 +46,126 @@ public class Mutation
             StartLocation = startLocation,
             EndLocation = endLocation,
             TotalSeats = totalSeats,
+            Driver = driverUser
         };
 
         await context.Groups.AddAsync(group, cancellationToken);
         await context.SaveChangesAsync(cancellationToken);
 
-        return group;
+        return context.Groups.Where(x => x == group);
     }
 
+    [Error<AuthenticationException>]
     [Error<ArgumentException>]
-    public async Task<Group?> DeleteGroup(
+    [UseMutationConvention(PayloadFieldName = "id")]
+    public async Task<int> DeleteGroup(
         int id,
         GroupContext context,
+        ClaimsPrincipal claimsPrincipal,
         CancellationToken cancellationToken
     )
     {
+        var userId =
+            claimsPrincipal.Identity?.Name
+            ?? throw new AuthenticationException("Failed to determine the current user's name");
+
+        var user =
+            await context.Users.FirstOrDefaultAsync(u => u.Id == userId, cancellationToken)
+            ?? throw new ArgumentException("User does not exist");
+
         var group =
             await context.Groups.FirstOrDefaultAsync(x => x.Id == id, cancellationToken)
             ?? throw new ArgumentException("Group does not exist", nameof(id));
 
+        if (group.Driver != user)
+            throw new ArgumentException("User is not the driver");
+
         context.Groups.Remove(group);
         await context.SaveChangesAsync(cancellationToken);
 
-        return group;
+        return group.Id;
+    }
+
+    [UseFirstOrDefault]
+    [UseProjection]
+    [Error<AuthenticationException>]
+    [Error<ArgumentException>]
+    public async Task<IQueryable<Group>> JoinGroup(
+        int id,
+        GroupContext context,
+        ClaimsPrincipal claimsPrincipal,
+        CancellationToken cancellationToken
+    )
+    {
+        var userId =
+            claimsPrincipal.Identity?.Name
+            ?? throw new AuthenticationException("Failed to determine the current user's name");
+
+        var user = await context.Users.FirstOrDefaultAsync(u => u.Id == userId, cancellationToken);
+
+        if (user == null)
+        {
+            user = new User { Id = userId };
+            await context.Users.AddAsync(user, cancellationToken);
+        }
+
+        var group =
+            await context
+                .Groups.Include(group => group.Driver)
+                .Include(group => group.Passengers)
+                .FirstOrDefaultAsync(x => x.Id == id, cancellationToken)
+            ?? throw new ArgumentException("Group does not exist", nameof(id));
+
+        if (user == group.Driver)
+            throw new ArgumentException("User is the driver");
+
+        if (group.Passengers.Any(passenger => passenger == user))
+            throw new ArgumentException("User is already one of the passengers");
+
+        if (group.Passengers.Count + 1 > group.TotalSeats)
+            throw new ArgumentException("There are no more seats left");
+
+        group.Passengers.Add(user);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return context.Groups.Where(x => x.Id == id);
+    }
+
+    [UseFirstOrDefault]
+    [UseProjection]
+    [Error<AuthenticationException>]
+    [Error<ArgumentException>]
+    public async Task<IQueryable<Group>> AbandonGroup(
+        int id,
+        GroupContext context,
+        ClaimsPrincipal claimsPrincipal,
+        CancellationToken cancellationToken
+    )
+    {
+        var userId =
+            claimsPrincipal.Identity?.Name
+            ?? throw new AuthenticationException("Failed to determine the current user's name");
+
+        var user =
+            await context.Users.FirstOrDefaultAsync(u => u.Id == userId, cancellationToken)
+            ?? throw new ArgumentException("User does not exist");
+
+        var group =
+            await context
+                .Groups.Include(group => group.Driver)
+                .Include(group => group.Passengers)
+                .FirstOrDefaultAsync(x => x.Id == id, cancellationToken)
+            ?? throw new ArgumentException("Group does not exist", nameof(id));
+
+        if (user == group.Driver)
+            throw new ArgumentException("User is the driver");
+
+        if (!group.Passengers.Any(passenger => passenger == user))
+            throw new ArgumentException("User is not one of the passengers");
+
+        group.Passengers.Remove(user);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return context.Groups.Where(x => x.Id == id);
     }
 }
--- services/GroupMaker/GroupMaker.Data/GroupContext.cs
+++ services/GroupMaker/GroupMaker.Data/GroupContext.cs
@@ -6,13 +6,18 @@ namespace GroupMaker.Api;
 public class GroupContext(DbContextOptions<GroupContext> context) : DbContext(context)
 {
     public DbSet<Group> Groups { get; set; }
+    public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         var groupEntity = builder.Entity<Group>();
-
         groupEntity.HasKey(group => group.Id);
         groupEntity.OwnsOne(x => x.StartLocation);
         groupEntity.OwnsOne(x => x.EndLocation);
+        groupEntity.HasOne(x => x.Driver).WithMany(x => x.Driving);
+        groupEntity.HasMany(x => x.Passengers).WithMany(x => x.Passengering);
+
+        var userEntity = builder.Entity<User>();
+        userEntity.HasKey(user => user.Id);
     }
 }
